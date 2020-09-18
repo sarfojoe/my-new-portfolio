ba7b4dacdc3c0933eda456dedf4c9f5a573e1806
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-09-18 23:26:27 +0300

    about page backgroung changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,18 @@ import About from "../components/About";
 import "./pages.css";
 
 function Home() {
-    return (
-        <div className="container">
-            <div className="heading-wrapper uk-child-width-expand@m uk-text-center uk-grid-collapse" uk-grid="true">
-                <HeadingText />
-                <Image />
-            </div>
-            <About />
-        </div>
-    );
+  return (
+    <div className="container">
+      <div
+        className="heading-wrapper uk-child-width-expand@m uk-text-center uk-grid-collapse"
+        uk-grid="true"
+      >
+        <HeadingText />
+        <Image />
+      </div>
+      <About />
+    </div>
+  );
 }
 
 export default Home;
